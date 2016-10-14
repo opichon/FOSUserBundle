b6c3b5d6e8eac9a01540341e178ab6048bc378a9
Olivier Pichon <op@united-asian.com>
2016-10-14 15:34:18 +0700

    Updated RegistrationController for extensibility

--- Controller/RegistrationController.php
+++ Controller/RegistrationController.php
@@ -96,7 +96,7 @@ class RegistrationController extends Controller
     /**
      * Tell the user to check his email provider.
      */
-    public function checkEmailAction()
+    public function checkEmailAction(Request $request)
     {
         $email = $this->get('session')->get('fos_user_send_confirmation_email/email');
 
@@ -159,7 +159,7 @@ class RegistrationController extends Controller
     /**
      * Tell the user his account is now confirmed.
      */
-    public function confirmedAction()
+    public function confirmedAction(Request $request)
     {
         $user = $this->getUser();
         if (!is_object($user) || !$user instanceof UserInterface) {
